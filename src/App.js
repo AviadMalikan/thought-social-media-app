0977db130c2dc938225ac96a45aa567b88b6c6fc
Aviad_Malikan <aviad879@gmail.com>
2024-08-28 12:05:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import Swal from 'sweetalert2';
+// import "sweetalert2/src/sweetalert2.scss"
 
 function App() {
+
+  function showMsg(isSuccess, msg) {
+
+    Swal.fire({
+      title: msg,
+      icon: isSuccess ? 'success' : 'error',
+      text: isSuccess ? '' : 'try again later',
+      showConfirmButton: false,
+      timer: 1200,
+      width: '250px',
+      position: 'bottom-end',
+      backdrop: 'rgba(0,0,123,0.0)',
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello react</h1>
+        <button onClick={() => showMsg(true, 'Remove successfully')}> OK</button>
       </header>
     </div>
   );
