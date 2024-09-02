e85515978692a33b1298369f630bf5f96c57538c
Aviad_Malikan <aviad879@gmail.com>
2024-09-02 18:20:00 +0300

    update the dated view

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -53,11 +53,11 @@ function showTimeTxt(time) {
 
     return day_diff == 0 && (
         diff < 60 && "just now" ||
-        diff < 120 && "1 minute ago" ||
-        diff < 3600 && Math.floor(diff / 60) + " minutes ago" ||
-        diff < 7200 && "1 hour ago" ||
-        diff < 86400 && Math.floor(diff / 3600) + " hours ago") ||
-        day_diff == 1 && "Yesterday" ||
-        day_diff < 7 && day_diff + " days ago" ||
-        day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago";
+        diff < 120 && "1m" ||
+        diff < 3600 && Math.floor(diff / 60) + "m" ||
+        diff < 7200 && "1h" ||
+        diff < 86400 && Math.floor(diff / 3600) + "h") ||
+        day_diff == 1 && "1d" ||
+        day_diff < 7 && day_diff + "d" ||
+        day_diff < 31 && Math.ceil(day_diff / 7) + "w";
 }
