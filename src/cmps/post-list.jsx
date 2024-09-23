56bedc8ffb2bc81f139dda999c511abe90a2be80
Aviad_Malikan <aviad879@gmail.com>
2024-09-23 14:23:25 +0300

    Deleted the function because it is no longer needed

--- src/cmps/post-list.jsx
+++ src/cmps/post-list.jsx
@@ -1,15 +1,14 @@
 import { PostPreview } from "./post-preview.jsx"
 
 
-export function PostList({ posts, onSelectPost, onRemovePost }) {
+export function PostList({ posts, onRemovePost }) {
 
 
     if (!posts) return <h2>loading...</h2>
     return <ul className="post-list">
         {posts.map(p => <li key={p.id}>
-            <PostPreview post={p} 
-            onRemovePost={onRemovePost}
-            onSelectPost={onSelectPost} />
+            <PostPreview post={p}
+                onRemovePost={onRemovePost} />
         </li>)}
     </ul>
 }
