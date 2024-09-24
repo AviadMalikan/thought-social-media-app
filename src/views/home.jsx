4a504bcd3405bf0a84617c55de48365061eefee9
Aviad_Malikan <aviad879@gmail.com>
2024-09-24 14:54:32 +0300

    change home page text

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -4,7 +4,7 @@ export function Home() {
             Hello To InstPOST!
         </h2>
         <h4>
-            You need to login-first
+            You need to login-to see more
         </h4>
     </section>
 }
